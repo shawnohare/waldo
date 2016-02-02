44424046930a63a594b457fea2da3584a5f3ab9c
Shawn O'Hare <ohare.shawn@gmail.com>
2016-02-02 10:25:24 -0800

    PairedComparison uses interfaces.

--- paired.go
+++ paired.go
@@ -1,14 +1,11 @@
 package waldo
 
-// PairedComparison tests whether two Bernoulli samples are drawn from
-// the same underlying distribution, utilizing a Wald test.  If X is the
-// number of successes in the first sample (of size m) and Y the same
-// value for the second sample (of size n),
-// then X ~ Binomial(m, p1) and Y ~ Binomial(n,p1)
-// for unkown parameter's p1 and p2.
+// PairedComparison tests whether two parameter estimates are equal.
+// A typical use-case is to determine whether two Bernoulli samples
+// are drawn from the same distribution.
 type PairedComparison struct {
-	X BernoulliSample
-	Y BernoulliSample
+	X Sample
+	Y Sample
 }
 
 // MLE estimate of the comparison estimator hat(p) := X.MLE() - Y.MLE()
@@ -22,13 +19,3 @@ func (s PairedComparison) Variance() float64 {
 	// V(X - Y) = V(X) + V(-Y) = V(X) + (-1)^2*V(Y)
 	return s.X.Variance() + s.Y.Variance()
 }
-
-// FIXME can probably delete this.
-// WaldStatistic associated to the comparison sample, using a Wald
-// test of size alpha.
-// func (s PairedComparison) WaldStatistic(alpha float64) *Statistic {
-// 	p := s.MLE()
-// 	se := StandardError(s)
-// 	t := Test{Alpha: alpha}
-// 	return t.Statistic(p, se)
-// }
